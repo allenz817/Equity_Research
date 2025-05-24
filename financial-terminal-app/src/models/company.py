b675a22b1307cb52544375804ebb41706e68e536
allenz817 <allenjzhao@gmail.com>
2025-05-24 17:02:44 +0800

    250524

--- financial-terminal-app/src/models/company.py
+++
@@ -1,7 +0,0 @@
-class Company:
-    def __init__(self, ticker: str, name: str):
-        self.ticker = ticker
-        self.name = name
-
-    def __repr__(self):
-        return f"Company(ticker='{self.ticker}', name='{self.name}')"
